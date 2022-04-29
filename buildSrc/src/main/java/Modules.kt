1ba42a01ef3e63c6b71fcbfa60e4470b28a1e578
Yovi E. Putra <yovi121093@gmail.com>
2022-04-30 05:57:01 +0700

    feat(init): new modules add feature and synchronize module

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,19 @@
+/**
+ * @created 30-04-2022 05:53
+ * @author yoviekaputra
+ * @project eFishery
+ **/
+
+object Modules {
+
+    object Core {
+        const val network = ":core:network"
+        const val abstractions = ":core:abstractions"
+        const val themes = ":core:themes"
+    }
+
+    object Features {
+        const val home = ":features:home"
+        const val syncronize = ":features:syncronize"
+    }
+}
