443bf9f39a99d4562445fe403401439c03cfb26d
Yovi E. Putra <yovi121093@gmail.com>
2022-05-01 05:27:49 +0700

    feat(init): database module add database local storage module for offline mode

---
+++ database/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp") version "1.6.10-1.0.2"
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        consumerProguardFiles(Config.consumerRulesLibrary)
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementationCore()
+
+    implementationTest()
+
+    implementationHilt()
+
+    implementationCoroutines()
+
+    implementationRoom()
+}
